65f42836cefe18621e527b1cdba533465865b80c
aldvs <andrewgogol777@gmail.com>
2022-01-29 23:18:45 +0300

    Update Model

--- NetworkingApp/Model.swift
+++ NetworkingApp/Model.swift
@@ -9,10 +9,20 @@ import Foundation
 
 struct Model: Decodable {
     
-    let name: String
-    let age: Int
-    let count: Int
+    let name: String?
+    let age: Int?
+    let count: Int?
     
+    init(name: String, age: Int, count: Int) {
+        self.name = name
+        self.age = age
+        self.count = count
+    }
     
+    init(fetchedData: [String: Any]) {
+        name = fetchedData["name"] as? String
+        age = fetchedData["age"] as? Int
+        count = fetchedData["count"] as? Int
+    }
     
 }
